bad13c96cef1a1eb82c2dd1ea7010876d0107113
unknown <obirikan020@gmail.com>
2022-09-26 21:10:15 +0000

    ++ notes

--- main.py
+++ main.py
@@ -25,20 +25,27 @@ y = np.array(data[predict])
 #testing and training using actual data to know correlation btn them
 #test_size is the percentage used for testing the remaining goes to training 
 x_train,x_test,y_train,y_test=sklearn.model_selection.train_test_split(x,y,test_size=0.5)
+
 #use the linearRegression module
 linear=linear_model.LinearRegression()
+
 #minimizing error(gradient decent)
 linear.fit(x_train,y_train)
+
 #test the accuracy of the error
 acc=linear.score(x_test,y_test)
+
 #printing out some values for verification
 print(acc)
 print('co: \n',linear.coef_)
 print('intercept: \n',linear.intercept_)
+
 #using my own data to test module
 v=[12 ,14 , 1 , 0 , 0]
+
 #predict the outcome of your value(s)
 predictions=linear.predict(x_test)
+
 #loop through prediction to see if your data is corresponding well
 for x in range(len(predictions)):
     print(predictions[x],x_test[x],y_test[x])
