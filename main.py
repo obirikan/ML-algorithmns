7fd53b62a9798b9443e3ed8e33cf00e1d0ac18cd
unknown <obirikan020@gmail.com>
2022-09-26 21:05:03 +0000

    adding notes

--- main.py
+++ main.py
@@ -7,18 +7,27 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
+# import Data
 data = pd.read_csv("student-mat.csv", sep=";")
 
+#take out the wanted labels / use wanted independent variable
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
+#dependent variable
 predict = "G3"
 
+#independent variable(s)
 x = np.array(data.drop([predict], 1))
+
+#dependent variable
 y = np.array(data[predict])
 
-
+#testing and training using actual data to know correlation btn them
+#test_size is the percentage used for testing the remaining goes to training 
 x_train,x_test,y_train,y_test=sklearn.model_selection.train_test_split(x,y,test_size=0.5)
+#use the linearRegression module
 linear=linear_model.LinearRegression()
+
 linear.fit(x_train,y_train)
 acc=linear.score(x_test,y_test)
 print(acc)
